d1b3e3992d7964eb115c45d9e2c15df3292b68e4
Serjkeey <sergeychoha2012@gmail.com>
2023-04-23 15:15:09 +0300

    Finished Homework_2.py

---
+++ Homework_2.py
@@ -0,0 +1,19 @@
+n = int(input('Введіть число:  '))
+
+if n % 2 != 0:
+    if n > 20:
+        print("Not Weird")
+    else:
+        print("Weird")
+else:
+    if 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+
+
+
+
+
+
+
