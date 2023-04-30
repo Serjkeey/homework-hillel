0b68f18609adc64cdf90ffcdcd24b1415f4985c8
Serjkeey <sergeychoha2012@gmail.com>
2023-04-30 22:27:02 +0300

    redid Homework_3.1.py

--- Homework_3.1.py
+++ Homework_3.1.py
@@ -1,13 +1,17 @@
-#Числа, які діляться на 3:
-enter_number = int(input('Введіть число:  '))
-for number in range(enter_number + 1):
-    if number % 3 == 0:
-        print(number)
+# Числа які діляться на 3:
+N = int(input("Введіть число : "))
+i = 0
+while i <= N:
+    if i % 3 == 0:
+        print(i)
+    i += 1
 
 #Числа, які діляться на 3 без залишку:
-enter_number = int(input('Введіть число:  '))
-summ = 0
-for number in range(enter_number + 1):
-    if number % 3 == 0:
-        summ += number
-        print(summ)
+N = int(input("Введіть число : "))
+i = 0
+sum = 0
+while i <= N:
+    if i % 3 == 0:
+        sum += i
+    i += 1
+print("Сумма чисел які діляться на 3 без залишку:  ", sum)
