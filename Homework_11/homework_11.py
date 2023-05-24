78fc742cf2029c897395f4ac6decd1738853a823
serjkeey <sergeychoha2012@gmail.com>
2023-05-24 19:35:57 +0300

    in process

---
+++ Homework_11/homework_11.py
@@ -0,0 +1,21 @@
+# 1
+# import pickle
+# # Створюємо словник з данними
+# contact = [
+#     {'Alex': '+380997654513'},
+#     {'Andrew': '+380675690061'},
+#     {'Serhii': '+380661238812'}
+# ]
+# file_name = 'contact.bin'
+# with open('contact.bin', 'wb') as file:
+#     pickle.dump(contact, file)
+# # Перевірка
+# test= open('contact.bin', 'rb')
+# contact = pickle.load(test)
+# print(contact)
+
+
+# 2
+import json
+A = {'key': 1}
+B = {'key1: 2}
