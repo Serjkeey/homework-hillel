3115b19eedff025fb86ae165d807ee3bfeb15e54
serjkeey <sergeychoha2012@gmail.com>
2023-05-29 13:16:24 +0300

    done!

--- Homework_11/homework_11.py
+++ Homework_11/homework_11.py
@@ -9,7 +9,7 @@
 # file_name = 'contact.bin'
 # with open('contact.bin', 'wb') as file:
 #     pickle.dump(contact, file)
-# # Перевірка
+# перевірка
 # test= open('contact.bin', 'rb')
 # contact = pickle.load(test)
 # print(contact)
@@ -17,5 +17,20 @@
 
 # 2
 import json
-A = {'key': 1}
-B = {'key1: 2}
+import json
+
+a = {'key': 1, 'key2': True}
+b = {'key': 'Hello'}
+c = {}
+for key, value in a.items():
+    if key in b:
+        c[key] = [value, b[key]]
+    else:
+        c[key] = value
+
+for key, value in b.items():
+    if key not in c:
+        c[key] = value
+
+with open('result.json', 'w') as file:
+    json.dump(c, file)
