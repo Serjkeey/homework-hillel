30e758d056a77c9f15ed657d32f51141e48f1ba7
serjkeey <sergeychoha2012@gmail.com>
2023-06-15 19:41:09 +0300

    add class contact

---
+++  Final work/address-book.py
@@ -0,0 +1,37 @@
+class contact:
+    def __init__(self, name, surname, phone_number, address='', birthday=''):
+        self.name = name
+        self.surname = surname
+        self.phone_number = phone_number
+        self.address = address
+        self.birthday = birthday
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_surname(self):
+        return self.surname
+
+    def set_surname(self, surname):
+        self.surname = surname
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def get_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    def get_address(self):
+        return self.address
+
+    def set_address(self, address):
+        self.address = address
+
+    def get_birthday(self):
+        return self.birthday
+
+    def set_birthday(self, birthday):
+        self.birthday = birthday
